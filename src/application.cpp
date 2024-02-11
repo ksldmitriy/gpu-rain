ba5a18fb189a6e5512cbdd6fbf7f08d11942a1e9
ksldmitriy <ksldmitriy@gmail.com>
2024-02-11 16:51:08 +0200

    application that renders image to each monitor implemented

--- src/application.cpp
+++ src/application.cpp
@@ -1,6 +1,13 @@
 #include "application.hpp"
 
-#include "glad/gl.h"
+#include "gl/shader.hpp"
+#include "programs-library.hpp"
+#include "render-area-layout-constructor.hpp"
+#include "shader-resources.hpp"
+
+#include <glad/gl.h>
+#include <iostream>
+#include <stb/stb_image.h>
 
 Application::Application() {
 }
@@ -21,17 +28,79 @@ void Application::Init() {
   m_window.CreateGLContext();
 
   m_window.GetMonitorDimensions();
+
+  CreateFramebuffer();
+
+  ProgramsLibrary::CreatePrograms();
+
+  std::vector<MonitorDimensions> monitor_dimentions =
+      m_window.GetMonitorDimensions();
+
+  RenderAreaLayoutConstructor render_area_constructor;
+  render_area_constructor.ConstructLayout(monitor_dimentions,
+                                          m_color_texture.GetSize(),
+                                          m_window.GetSize(), m_render_area);
+
+  m_image_renderer.Create(s_test_media_path);
 }
 
 void Application::MainLoop() {
   while (true) {
-    glClearColor(0.12f, 0.12f, 0.12f, 0.12f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
+    DrawToFrambuffer();
+    DrawToScreen();
     m_window.SwapBuffers();
   }
+}
+
+void Application::DrawToFrambuffer() {
+  m_framebuffer.Bind();
+  SetViewport(m_color_texture.GetSize());
+
+  glClearColor(1.0f, 0.0f, 0.0f, 0.12f);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  m_image_renderer.Draw();
+}
+
+void Application::DrawToScreen() {
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  SetViewport(m_window.GetSize());
+
+  glClearColor(1.0f, 0.0f, 0.0f, 0.12f);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  ProgramsLibrary::GetTextureProgram().Use();
+  m_render_area.vao.Bind();
+  m_render_area.ebo.Bind();
+
+  m_color_texture.Bind();
+
+  glDrawElements(GL_TRIANGLES, m_render_area.index_count, GL_UNSIGNED_INT, 0);
 }
 
 void Application::Cleanup() {
   m_window.Close();
 }
+
+void Application::CreateFramebuffer() {
+  gl::TextureCreateInfo create_info;
+  create_info.format = GL_RGB;
+  create_info.internal_format = GL_RGB;
+  create_info.min_filter = GL_LINEAR;
+  create_info.mag_filter = GL_LINEAR;
+  create_info.wrap_s = GL_CLAMP_TO_EDGE;
+  create_info.wrap_t = GL_CLAMP_TO_EDGE;
+
+  m_color_texture.Create(glm::uvec2(1920, 1080), create_info, nullptr);
+
+  m_color_texture.GetHandle();
+
+  m_framebuffer.Create();
+  m_framebuffer.AttachTexture(m_color_texture, GL_COLOR_ATTACHMENT0);
+}
+
+void Application::SetViewport(glm::uvec2 size) {
+  glViewport(0, 0, size.x, size.y);
+}
+
+const char *const Application::s_test_media_path = "media/image.jpg";
--- src/application.hpp
+++ src/application.hpp
@@ -1,4 +1,11 @@
 #pragma once
+#include "gl/framebuffer.hpp"
+#include "gl/program.hpp"
+#include "gl/texture.hpp"
+#include "gl/vao.hpp"
+#include "gl/vbo.hpp"
+#include "image-renderer.hpp"
+#include "render-area-layout.hpp"
 #include "x-root-window.hpp"
 
 class Application {
@@ -13,7 +20,24 @@ private:
   void Init();
   void MainLoop();
   void Cleanup();
-  
+
+  void DrawToFrambuffer();
+  void DrawToScreen();
+
+  void CreateFramebuffer();
+
+  void SetViewport(glm::uvec2 size);
+
 private:
   XRootWindow m_window;
+
+  gl::Framebuffer m_framebuffer;
+  gl::Texture m_color_texture;
+
+  gl::Texture m_image_texture;
+
+  ImageRenderer m_image_renderer;
+  RenderAreaLayout m_render_area;
+
+  static const char *const s_test_media_path;
 };
